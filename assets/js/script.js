109c79261be34d7ec23fdfd8913841c47c1391c3
mcallisterliza <mcallisterliza@outlook.com>
2024-02-15 12:31:04 +0000

    Add another 20 questions to questionsBank and implement the Fisher-Yates shuffle to enable quiz to shuffle and select 10 questions at random each time the user decides to play the quiz again allowing for a better user experience

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,159 @@
 /* jshint esversion: 8 */
 /*
- *Questions array for quiz
+ *Questions bank array for quiz of 30 questions. Quiz will randomly select 10 questions each time
  */
-const questions = [{
-        question: "Q.1 What is the symbol of love?",
+const questionsBank = [{
+        question: "What is the symbol of love?",
         options: ["Heart", "Star", "Circle", "Square"],
         correctAnswer: "Heart"
     },
     {
-        question: "Q.2 What flower is often associated with love and romance?",
+        question: "What flower is often associated with love and romance?",
         options: ["Daisy", "Rose", "Tulip", "Sunflower"],
         correctAnswer: "Rose"
     },
     {
-        question: "Q.3 Which romantic movie features the characters Noah and Allie and is based on a Nicholas Sparks novel?",
+        question: "Which romantic movie features the characters Noah and Allie and is based on a Nicholas Sparks novel?",
         options: ["The Notebook", "Titanic", "Pride and Prejudice", "Twilight"],
         correctAnswer: "The Notebook"
     },
     {
-        question: "Q.4 What is the traditional gift for a 25th wedding anniversary?",
+        question: "What is the traditional gift for a 25th wedding anniversary?",
         options: ["Silver", "Gold", "Ruby", "Diamond"],
         correctAnswer: "Silver"
     },
     {
-        question: "Q.5 In which month is Valentine's Day celebrated?",
+        question: "In which month is Valentine's Day celebrated?",
         options: ["February", "January", "March", "April"],
         correctAnswer: "February"
     },
     {
-        question: "Q.6 What is the Italian word for love?",
+        question: "What is the Italian word for love?",
         options: ["Amore", "Ciao", "Grazie", "Pizza"],
         correctAnswer: "Amore"
     },
     {
-        question: "Q.7 Which famous playwright wrote 'Romeo and Juliet'?",
+        question: "Which famous playwright wrote 'Romeo and Juliet'?",
         options: ["William Shakespeare", "Jane Austen", "Charles Dickens", "F. Scott Fitzgerald"],
         correctAnswer: "William Shakespeare"
     },
     {
-        question: "Q.8 What is the love hormone that plays a role in social bonding and affection?",
+        question: "What is the love hormone that plays a role in social bonding and affection?",
         options: ["Oxytocin", "Dopamine", "Serotonin", "Endorphin"],
         correctAnswer: "Oxytocin"
     },
     {
-        question: "Q.9 What date in the month is Valentine's Day?",
+        question: "What date in the month is Valentine's Day?",
         options: ["12th", "13th", "14th", "15th"],
         correctAnswer: "14th"
     },
     {
-        question: "Q.10 Which city is known as the 'City of Love'?",
+        question: "Which city is known as the 'City of Love'?",
         options: ["Paris", "Rome", "Venice", "Barcelona"],
         correctAnswer: "Paris"
     },
+    {
+        question: "What is the traditional gift for a 50th wedding anniversary?",
+        options: ["Gold", "Diamond", "Silver", "Emerald"],
+        correctAnswer: "Gold"
+    },
+    {
+        question: "In which year did the tradition of sending Valentine's Day cards begin?",
+        options: ["1800s", "1600s", "1900s", "1700s"],
+        correctAnswer: "1700s"
+    },
+    {
+        question: "What is the name of the winged god of love in ancient Roman mythology?",
+        options: ["Cupid", "Eros", "Amor", "Apollo"],
+        correctAnswer: "Cupid"
+    },
+    {
+        question: "Which bird is often associated with love and mating for life?",
+        options: ["Swan", "Peacock", "Dove", "Parrot"],
+        correctAnswer: "Swan"
+    },
+    {
+        question: "What popular sweet treat is often given on Valentine's Day and has phrases written on them?",
+        options: ["Chocolate Hearts", "Candy Canes", "Love Hearts", "Jelly Beans"],
+        correctAnswer: "Love Hearts"
+    },
+    {
+        question: "What romantic novel features the characters Elizabeth Bennet and Mr. Darcy?",
+        options: ["Pride and Prejudice", "Wuthering Heights", "Sense and Sensibility", "Jane Eyre"],
+        correctAnswer: "Pride and Prejudice"
+    },
+    {
+        question: "What is the meaning of the red rose, the most iconic symbol of Valentine's Day?",
+        options: ["Friendship", "Passion", "Peace", "Gratitude"],
+        correctAnswer: "Passion"
+    },
+    {
+        question: "Which ancient Roman festival is believed to be the origin of Valentine's Day?",
+        options: ["Lupercalia", "Saturnalia", "Ides of March", "Kalends"],
+        correctAnswer: "Lupercalia"
+    },
+    {
+        question: "Who is the Roman goddess of love and beauty?",
+        options: ["Aphrodite", "Venus", "Isis", "Hera"],
+        correctAnswer: "Venus"
+    },
+    {
+        question: "In the language of flowers, what does a pink rose symbolize?",
+        options: ["Adoration", "Gratitude", "Friendship", "Love"],
+        correctAnswer: "Love"
+    },
+    {
+        question: "What Shakespearean play features the famous quote 'The course of true love never did run smooth'?",
+        options: ["Romeo and Juliet", "A Midsummer Night's Dream", "Macbeth", "Hamlet"],
+        correctAnswer: "A Midsummer Night's Dream"
+    },
+    {
+        question: "What is the significance of the Love Knot in relation to weddings?",
+        options: ["Symbol of eternal love", "Unity of two families", "Promise of fidelity", "Good luck and prosperity"],
+        correctAnswer: "Symbol of eternal love"
+    },
+    {
+        question: "In Japan, what do women traditionally give to men on Valentine's Day?",
+        options: ["Chocolates", "Flowers", "Love letters", "Jewelry"],
+        correctAnswer: "Chocolates"
+    },
+    {
+        question: "Who is known as the patron saint of lovers?",
+        options: ["St. Patrick", "St. Valentine", "St. Nicholas", "St. George"],
+        correctAnswer: "St. Valentine"
+    },
+    {
+        question: "What romantic novel features the characters Scarlett O'Hara and Rhett Butler?",
+        options: ["Pride and Prejudice", "Gone with the Wind", "Wuthering Heights", "Sense and Sensibility"],
+        correctAnswer: "Gone with the Wind"
+    },
+    {
+        question: "What is the name of the love song sung by Elvis Presley?",
+        options: ["Love Me Tender", "Can't Help Falling in Love", "Unchained Melody", "Something"],
+        correctAnswer: "Can't Help Falling in Love"
+    },
+    {
+        question: "What is the traditional gift for a 15th wedding anniversary?",
+        options: ["Crystal", "Pearl", "Ivory", "Platinum"],
+        correctAnswer: "Crystal"
+    },
+    {
+        question: "Which romantic comedy film stars Julia Roberts as a popular escort in Los Angeles?",
+        options: ["Pretty Woman", "My Best Friend's Wedding", "Notting Hill", "Runaway Bride"],
+        correctAnswer: "Pretty Woman"
+    },
+    {
+        question: "What is the Italian dessert often associated with love, meaning 'pick me up'?",
+        options: ["Tiramisu", "Cannoli", "Panna Cotta", "Gelato"],
+        correctAnswer: "Tiramisu"
+    },
+    {
+        question: "What does the term 'betrothal' refer to in the context of love and marriage?",
+        options: ["Engagement", "Wedding ceremony", "Honeymoon", "Anniversary"],
+        correctAnswer: "Engagement"
+    },
 ];
+
 
 /**
  *  Get references to various elements using IDs
@@ -71,7 +172,6 @@ let currentQuestion = 0;
 /**
  *  Represents the number of correct answers obtained by the user
  */
-
 let score = 0;
 
 
@@ -111,9 +211,27 @@ logo.addEventListener("click", function () {
 });
 
 /**
+ * the following function uses Fisher-Yates (Knuth) Shuffle algorithm to shuffle the questions 
+ * so that the user has a new set of questions each time they play the quiz
+ */
+function shuffleArray(array) {
+    return array.sort(() => Math.random() - 0.5);
+}
+
+/**
  *  Functions used to run quiz game
  */
 function startQuiz() {
+
+    /**
+     * Shuffle the questionsBank array
+     */
+    const shuffledQuestions = shuffleArray(questionsBank);
+
+    /**
+     * Set the questions array to the first 10 questions
+     */
+    questions = shuffledQuestions.slice(0, 10);
     /**
      *  current question index and score
      */
